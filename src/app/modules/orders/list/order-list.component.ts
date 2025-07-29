2f3611e9223b52c5c66a2e2f242dd016671f1580
APTTUS/srajeshwari <srajeshwari@conga.com>
2025-07-29 07:31:30 +0530

    Template changes-202507.2.0

--- src/app/modules/orders/list/order-list.component.ts
+++ src/app/modules/orders/list/order-list.component.ts
@@ -82,15 +82,15 @@ export class OrderListComponent implements OnInit, OnDestroy {
                   mobileView: true
                 },
                 {
-                  prop: 'PriceList.Name',
+                  prop: 'PriceList',
                   label: 'CUSTOM_LABELS.PRICELIST'
                 },
                 {
-                  prop: 'BillToAccount.Name',
+                  prop: 'BillToAccount',
                   label: 'CUSTOM_LABELS.BILL_TO'
                 },
                 {
-                  prop: 'ShipToAccount.Name',
+                  prop: 'ShipToAccount',
                   label: 'CUSTOM_LABELS.SHIP_TO'
                 },
                 {
