ae500e98706d35cfdcd8c0ddcc1ae71684052d98
APTTUS/srajeshwari <srajeshwari@conga.com>
2025-11-17 11:37:08 +0530

    Template changes - 202511.1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { TranslateService } from '@ngx-translate/core';
 import { combineLatest, Observable, Subject } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, takeUntil, filter } from 'rxjs/operators';
 import { get } from 'lodash';
 import { BatchSelectionService } from '@congarevenuecloud/elements';
 @Component({
@@ -17,13 +19,27 @@ export class AppComponent implements OnInit, OnDestroy {
   showDrawer$: Observable<boolean>;
   private readonly _destroying$ = new Subject<void>();
 
-  constructor(private batchSelectionService: BatchSelectionService) { }
-
+   constructor(
+    private batchSelectionService: BatchSelectionService, 
+    private titleService: Title, 
+    private translateService: TranslateService
+  ) { }
+  
   ngOnInit() {
+    this.setTranslatedTitle();
     this.showDrawer$ = combineLatest([
       this.batchSelectionService.getSelectedProducts(),
       this.batchSelectionService.getSelectedLineItems()
     ]).pipe(map(([productList, lineItemList]) => get(productList, 'length', 0) > 0 || get(lineItemList, 'length', 0) > 0));
+  }
+
+  private setTranslatedTitle(): void {
+    this.translateService.get('APP.TITLE')
+      .pipe(
+        filter(title => title !== 'APP.TITLE'),
+        takeUntil(this._destroying$)
+      )
+      .subscribe(title => this.titleService.setTitle(title as string));
   }
 
   ngOnDestroy(): void {
